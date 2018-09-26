6a7f1fe892105c1013e38bcc1261c83a4dff5955
pathakmanu <mp4515@nyu.edu>
2018-09-26 16:09:17 -0400

    Add files via upload

---
+++ HW3_mp4515/show_bus_locations_mp4515.py
@@ -0,0 +1,37 @@
+
+# coding: utf-8
+
+# In[9]:
+
+
+import os
+import matplotlib
+import json
+import sys
+try:
+    import urllib2 as urllib
+except ImportError:
+    import urllib.request as urllib
+    
+if not len(sys.argv) == 3:
+    print ("Invalid number of arguments. Run as: python aPythonScriptThatWritesToCSV.py mtakey busline mycvs.csv")
+    sys.exit()
+
+url= "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key="+sys.argv[1]+"&VehicleMonitoringDetailLevel=calls&LineRef="+sys.argv[2]
+response = urllib.urlopen(url)
+data = response.read().decode("utf-8")
+data = json.loads(data)
+
+data1=data["Siri"]["ServiceDelivery"]["VehicleMonitoringDelivery"]
+data2=data1[0]["VehicleActivity"]
+x = data2[0]['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
+y = data2[0]['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
+lat=[x]
+long=[y]
+for i in range(len(data2)):
+    #print("Bus"+ "is at latitude {} and longitude {}").format(x,y)
+    lat.append(x)
+    long.append(y)
+for i in range(len(data2)):
+    print("Bus",i,"is at latitude ", lat[i], "and longitude ", long[i])
+
